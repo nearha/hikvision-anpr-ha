d0bbc1883cda082d8db7b874b36ea31e77d9e71a
nearha <114181979+nearha@users.noreply.github.com>
2026-06-22 10:18:56 -0300

    Fix complete fast metadata event entity implementation

--- custom_components/hikvision_anpr/event.py
+++ custom_components/hikvision_anpr/event.py
@@ -8,5 +8,4 @@ from typing import Any
 from homeassistant.components.event import EventEntity, EventEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import callback
-from homeassistant.helpers.restore_state import RestoreEntity
-from
+from homeassistant
